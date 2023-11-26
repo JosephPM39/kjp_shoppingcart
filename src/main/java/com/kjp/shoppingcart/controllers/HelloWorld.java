2d6044bbc7fb8fe511230840010cf6853875ffd3
JosephPM39 <josias.jjpm49@hotmail.com>
2023-11-25 20:58:19 -0600

    Improving devcontainer and using keycloak with spring boot, now it's missing to have a full keycloak integration with user endpoints

--- src/main/java/com/kjp/shoppingcart/controllers/HelloWorld.java
+++ src/main/java/com/kjp/shoppingcart/controllers/HelloWorld.java
@@ -1,16 +1,26 @@
 package com.kjp.shoppingcart.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
 public class HelloWorld {
 
-    @GetMapping
-    public String g(){
-        return "Hello world";
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+
+    @GetMapping("/admin")
+    public String helloAdmin(){
+        return "Hello Admin";
+    }
+
+    @GetMapping("/user")
+    public String helloUser(){
+        return "Hello User";
     }
 }
